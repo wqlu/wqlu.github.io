f8241243d1577f529c986be6181a9e9c2491fee7
simon-lu <wq.lu@outlook.com>
2020-09-18 22:24:28 +0800

    add pangu js

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,14 @@
 
 commit_message=$1
 
-SITE_SOURCE="/home/lu/my_blog"
-PUBLIC_DIR="/home/lu/code/git_project/simon-lu.github.io"
-cd "${SITE_SOURCE}"
-rm -rf "${SITE_SOURCE}/"public/*
+if [-d public ];then
+    rm -rf public
+else
+    echo "public not exist"
+fi
 
 git add .
 git commit -m "${commit_message}"
-git push origin master 
+git push origin blog
 
-echo "上传源文件成功"
-
-# 将 vec 修改为你的主题名
-hugo || exit 1
-rm -rf "${PUBLIC_DIR}/"* && cp -R "${SITE_SOURCE}/public/"* "${PUBLIC_DIR}/"
-cd "${PUBLIC_DIR}"
-git add .
-git commit -m "update blog"
-git push origin master --force
-
-echo "网站部署成功"
+echo "源码上传成功"
